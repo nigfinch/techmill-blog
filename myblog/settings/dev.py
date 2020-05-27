df5a6ec338af60d46878cbc30c2a3d463500f865
Nigel Finch <nigfinch@gmail.com>
2020-05-27 21:12:46 +0100

    First commit from dev env

---
+++ myblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '#h82qp=+s+++rwxr=kn9(=@83(9r^t&m(g52xre%l38&d*tqa('
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
