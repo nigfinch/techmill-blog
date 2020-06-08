acbd16b17f16766aeabda562f6374fdfdde1d33a
Nigel Finch <nigfinch@gmail.com>
2020-06-08 20:59:16 +0100

    fix avatars, redirect from homepage to blog index

--- home/models.py
+++ home/models.py
@@ -4,6 +4,10 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+# redirect from homepage to blog. We can add a website hompege later if required 
+from django.http import HttpResponseRedirect
+
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
@@ -11,3 +15,10 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ]
+
+    def serve(self, request):
+        # Redirect to blog index page
+        return HttpResponseRedirect('/blog/')
+
+        # only do this if you're using urls.py and namespaces
+        # return HttpResponseRedirect(reverse('blog:index'))
