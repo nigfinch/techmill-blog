203768a6f1b8ac95b87427537cfdc4a55b654913
Nigel Finch <nigfinch@gmail.com>
2020-06-20 18:18:46 +0100

    whitenoise

--- myblog/settings/base.py
+++ myblog/settings/base.py
@@ -154,7 +154,8 @@ STATICFILES_DIRS = [
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
