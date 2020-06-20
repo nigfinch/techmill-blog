a3c3b4da1f07a0d0d28d02c3e562fb20d8e6a52f
Nigel Finch <nigfinch@gmail.com>
2020-06-20 16:50:12 +0100

    Some improvments

--- myblog/settings/dev.py
+++ myblog/settings/dev.py
@@ -12,6 +12,7 @@ ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
+
 try:
     from .local import *
 except ImportError:
--- myblog/settings/production.py
+++ myblog/settings/production.py
@@ -1,6 +1,17 @@
 from .base import *
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
 
 DEBUG = False
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '#h82qp=+s+++rwxr=kn9(=@83(9r^t&m(g52xre%l38&d*tqa('
 
 try:
     from .local import *
