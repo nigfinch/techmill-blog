621d9f61e70aa3854a4429bb97e5efc519556b16
Nigel Finch <nigfinch@gmail.com>
2020-06-21 12:38:52 +0100

    AWS Bucket for Media storage

--- myblog/urls.py
+++ myblog/urls.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-from django.conf.urls.static import static
 
 from search import views as search_views
 
@@ -17,7 +16,7 @@ urlpatterns = [
 
     url(r'^search/$', search_views.search, name='search'),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 
 if settings.DEBUG:
