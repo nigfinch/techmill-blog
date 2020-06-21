58a94af2423871f6b11a5d8e2df51c31e4f96c1e
Nigel Finch <nigfinch@gmail.com>
2020-06-21 12:07:59 +0100

    AWS Bucket for Media storage

--- myblog/settings/base.py
+++ myblog/settings/base.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = [
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
